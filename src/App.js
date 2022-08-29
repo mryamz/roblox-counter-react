8109d70a22d377e0c8805108825b273c91c79c51
teddy-clapp <teddy@quadrata.io>
2022-08-28 18:03:46 -0700

    new

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+const axios = require('axios');
 
 function App() {
+
+  const getSig = (address, nonce) => {
+    Axios.get(`https://3dq0uoq813.execute-api.us-east-1.amazonaws.com/users/${address}/${nonce}`).then(response => {
+      const {sig} = response.data;
+      console.log("FROM API: ")
+      console.log(sig)
+    })
+  }
+
+  getSig('0x1817F384f0344d4699A1a28b8d5c61F7df6Fd470', 0);
+  getSig('0x1817F384f0344d4699A1a28b8d5c61F7df6Fd470', 1);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Roblox Counter Dapp
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );

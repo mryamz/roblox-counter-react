0de4214bf4e5ee6f7ae7327c19fd3bd13217aeb3
teddy-clapp <teddy@quadrata.io>
2022-08-28 21:28:55 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ function App() {
 
   const getSig = (address, nonce) => {
     axios.get(`https://3dq0uoq813.execute-api.us-east-1.amazonaws.com/users/${address}/${nonce}`, {
-      headers: {
-        'Access-Control-Allow-Origin' : '*',
-        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-      }
     }).then(response => {
       const {sig} = response.data;
       console.log("FROM API: ")
